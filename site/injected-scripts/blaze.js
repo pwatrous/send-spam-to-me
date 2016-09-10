0a13eb6e111546aa6fa8dff9dab44106be8bab85
sl <tyu.sam@gmail.com>
2016-09-10 18:42:04 -0400

    changed jquery scripts to on load

--- site/injected-scripts/blaze.js
+++ site/injected-scripts/blaze.js
@@ -1,7 +1,9 @@
 function () {
-	var email = '%%EMAIL_ADDRESS%%';
-	$('#user_email1').val(email);
-	$('#user_email2').val(email);
-	$('#zip_code').val('03301');
-	$('#wp-submit').click();
+  $(document).ready(function() {
+    var email = '%%EMAIL_ADDRESS%%';
+    $('#user_email1').val(email);
+    $('#user_email2').val(email);
+    $('#zip_code').val('03301');
+    $('#wp-submit').click();
+  });
 }
--- site/injected-scripts/flowers.js
+++ site/injected-scripts/flowers.js
@@ -1,5 +1,7 @@
 function () {
-  var email = '%%EMAIL_ADDRESS%%';
-  $('.form-control').val(email);
-  $('.btn-primary').click();
+  $(document).ready(function() {
+    var email = '%%EMAIL_ADDRESS%%';
+    $('.form-control').val(email);
+    $('.btn-primary').click();
+  });
 }
