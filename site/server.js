c2daffdcf800dc13e479dd291fdaa925eee26678
sl <tyu.sam@gmail.com>
2016-09-10 10:38:15 -0400

    made server serve files other than index.html... yeah...

--- site/server.js
+++ site/server.js
@@ -1,5 +1,7 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
+var path = require('path');
 var formidable = require('formidable');
 var util = require('util');
 
@@ -8,11 +10,46 @@ Serving of root html page
 */
 var server = http.createServer(function (req, res) {
     if (req.method.toLowerCase() == 'get') {
-      displayForm(res);
+      serveHTML(req, res);
     } else if (req.method.toLowerCase() == 'post') {
       processFormFieldsIndividual(req, res);
     }
 });
+
+function serveHTML(req, res) {
+  var uri = url.parse(req.url).pathname
+  var filename = path.join(process.cwd(), uri);
+  fs.exists(filename, function(exists) {
+    if (!exists) {
+      /*
+      res.writeHead(404, {"Content-Type": "text/plain"});
+      res.write("404 Not Found\n");
+      res.end();
+      */
+      fs.readFile('404.html', function (err, data) {
+        res.writeHead(404, {
+          'Content-Type': 'text/html',
+          'Content-Length': data.length
+        });
+        res.write(data);
+        res.end();
+      });
+      return;
+    }
+    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+    fs.readFile(filename, "binary", function(err, file) {
+      if(err) {        
+        res.writeHead(500, {"Content-Type": "text/plain"});
+        res.write(err + "\n");
+        res.end();
+        return;
+      }
+      res.writeHead(200);
+      res.write(file, "binary");
+      res.end();
+    });
+  });
+}
 
 function displayForm(res) {
     fs.readFile('index.html', function (err, data) {
